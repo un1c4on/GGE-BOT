538465b1ea92e0cb3688d07135f6335f440bab85
darrenthebozz <dazwallcroft123@gmail.com>
2025-12-22 06:24:45 +0000

    Code cleanup

--- plugins/timer.js
+++ plugins/timer.js
@@ -1,5 +1,4 @@
-const { isMainThread, parentPort, threadId } = require('node:worker_threads');
-const { botConfig } = require("../ggebot")
+const { isMainThread } = require('node:worker_threads')
 const name = "Timer"
 
 if (isMainThread) {
@@ -15,20 +14,20 @@ if (isMainThread) {
             },
         ]
     };
-    // const {loggedInUsers} = require("../main")
     return
 }
 
-const pluginOptions = botConfig.plugins[require('path').basename(__filename).slice(0, -3)] ??= {}
-
-//Check time here and kill if ahead
-
+const { botConfig } = require("../ggebot")
 const sqlite3 = require("sqlite3")
 const {webSocket} = require("../ggebot")
 
-let userDatabase = new sqlite3.Database("./user.db", sqlite3.OPEN_READWRITE)
+const pluginOptions = botConfig.plugins[require('path').basename(__filename).slice(0, -3)] ??= {}
+
+const userDatabase = new sqlite3.Database("./user.db", sqlite3.OPEN_READWRITE)
+
 if(isNaN(Number(pluginOptions.hours)))
     return console.log(`[${name}] hours is not a number!`)
+
 setTimeout(() => {
     userDatabase.run(`UPDATE SubUsers SET state = ? WHERE id = ?`, [0, botConfig.id], _ => {
         userDatabase.close()
