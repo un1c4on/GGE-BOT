6f6581785e163e4905b498c413302ab293e3a6c6
darrenthebozz <darrenthebozz1@gmail.com>
2025-12-29 13:10:40 +0000

    Initialize git repository if not already initialized

--- start.sh
+++ start.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
 URL="http://127.0.0.1:3001"
 
+if [ ! -d ".git" ]; then
+  git init -b main
+  git remote add origin https://github.com/darrenthebozz/GGE-BOT.git
+  git add .
+  git pull origin main
+fi
+
 git pull --recurse-submodules
 npm i
  
