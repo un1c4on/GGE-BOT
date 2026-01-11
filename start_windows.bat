4870d0d4ba0d7f4946e5c63b452ff91d35671c9e
darrenthebozz <dazwallcroft123@gmail.com>
2026-01-11 16:42:11 +0000

    Sort the guys issues

--- start_windows.bat
+++ start_windows.bat
@@ -1,5 +1,6 @@
 @echo off
-
+@REM Prevent run as admin issues
+cd /D "%~dp0"
 if not exist ".git"\ (
   git init -b main >NUL 2>&1
   git remote add origin https://github.com/darrenthebozz/GGE-BOT.git >NUL 2>&1
